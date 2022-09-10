e8b8ac4e1c0b051904295031aaa9f5eb02251433
RKabir <taposkabir@yahoo.com>
2022-09-11 03:39:21 +0600

    initial setup and resources

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            background: '#EEEDDE',
+            orange: '#FF7527',
+            dark: '#203239',
+        },
+        maxWidth: {
+            container: "1166px",
+        },
+        fontFamily: {
+            pop: "'Poppins', sans-serif",
+            rob: "'Roboto', sans-serif",
+        },
+      },
+    },
+  };
