f7f94bca107e5ee7183c5d70c36fa6d51da1537c
Md. Mehedi Hasan Akash <95402186+mehediakash@users.noreply.github.com>
2022-09-11 14:40:37 +0600

    nav banner part add

---
+++ js/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        textColor: "#203239",
+      },
+      fontFamily: {
+        pop: "'Poppins', sans-serif",
+      },
+      backgroundColor: {
+        bannerbg: "#EEEDDE",
+      },
+      maxWidth: {
+        container: "1166px",
+      },
+    },
+  },
+};
