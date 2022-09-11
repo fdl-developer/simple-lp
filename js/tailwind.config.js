5a2884167aec346d57c91b2b3ffebe9f0edfe256
dm-abu-hasib <dmabuhasib362000@gmail.com>
2022-09-11 14:51:41 +0600

    custom-css-added

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend:{
+            maxwidth:{
+                container: '1166px'
+            },
+            fontFamily:{
+                pop:"'Poppins', sans-serif",
+                com:"'Combo', cursive",
+            },
+            colors: {
+                orange:'#FF7527',
+                black: '#203239',
+                gray: '#D6D5C7',
+                
+            },
+
+        },
+    },
+};
