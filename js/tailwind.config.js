569e10e8f1497a2892b9bc3bebbba7ad0c268341
Md. Mehedi Hasan Akash <95402186+mehediakash@users.noreply.github.com>
2022-09-13 03:20:04 +0600

    Compelet without responsive

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -3,24 +3,25 @@ tailwind.config = {
     extend: {
       colors: {
         textColor: "#203239",
-        orange:'#FF7527',
-        black: '#203239',
-        gray: '#D6D5C7', 
-        dark: '#203239',
-        background: '#EEEDDE',
-            },
+        orange: "#FF7527",
+        orangeLight: "#FF752733",
+        black: "#203239",
+        gray: "#D6D5C7",
+        dark: "#203239",
+        background: "#EEEDDE",
+      },
       fontFamily: {
         pop: "'Poppins', sans-serif",
-        com:"'Combo', cursive",
+        com: "'Combo', cursive",
         rob: "'Roboto', sans-serif",
       },
       backgroundColor: {
         bannerbg: "#EEEDDE",
       },
-        backgroundImage: {
-          residen1: "url('images/residen1.png')",
-          residen2: "url('images/residen2.png')",
-        },
+      backgroundImage: {
+        residen1: "url('images/residen1.png')",
+        residen2: "url('images/residen2.png')",
+      },
       maxWidth: {
         container: "1166px",
       },
