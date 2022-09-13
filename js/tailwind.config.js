6b23e428f7f1fca092af10788e951935e83fdbfc
Md. Mehedi Hasan Akash <95402186+mehediakash@users.noreply.github.com>
2022-09-13 20:37:08 +0600

    er

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,42 +1,37 @@
 tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        textColor: "#203239",
-        orange: "#FF7527",
-<<<<<<< Updated upstream
-        orangeLight: "#FF752733",
-=======
->>>>>>> Stashed changes
-        black: "#203239",
-        gray: "#D6D5C7",
-        dark: "#203239",
-        background: "#EEEDDE",
-<<<<<<< Updated upstream
-=======
-        orangelight: "#F1D5B9",
->>>>>>> Stashed changes
-      },
-      fontFamily: {
-        pop: "'Poppins', sans-serif",
-        com: "'Combo', cursive",
-        rob: "'Roboto', sans-serif",
-      },
-      backgroundColor: {
-        bannerbg: "#EEEDDE",
-      },
+  extend: {
+    colors: {
+      textColor: "#203239",
+      orange:'#FF7527',
+      black: '#203239',
+      gray: '#D6D5C7', 
+      dark: '#203239',
+      background: '#EEEDDE',
+          },
+      orange: "#FF7527",
+      orangeLight: "#FF752733",
+      black: "#203239",
+      gray: "#D6D5C7",
+      dark: "#203239",
+      background: "#EEEDDE",
+    },
+    fontFamily: {
+      pop: "'Poppins', sans-serif",
+      com:"'Combo', cursive",
+      com: "'Combo', cursive",
+      rob: "'Roboto', sans-serif",
+    },
+    backgroundColor: {
+      bannerbg: "#EEEDDE",
+    },
       backgroundImage: {
         residen1: "url('images/residen1.png')",
         residen2: "url('images/residen2.png')",
-<<<<<<< Updated upstream
-=======
-        footer: "url('images/footer-bg.png')",
-        last: "url('images/last.png')",
->>>>>>> Stashed changes
       },
-      maxWidth: {
-        container: "1166px",
-      },
+    backgroundImage: {
+      residen1: "url('images/residen1.png')",
+      residen2: "url('images/residen2.png')",
     },
-  },
-};
+    maxWidth: {
+      container: "1166px",
+    },
